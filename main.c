6c2544faf3469ea76fcf7ea31f78345315444094
AlexMorrison <Valenciavitebsk.1994@mail.ru>
2013-04-03 20:34:25 +0300

    First Commit

---
+++ main.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<stdio_ext.h>
+
+#define max_length 30
+#define max_base 20
+
+int main (int argc, char **argv)
+{
+    return 0;
+}
